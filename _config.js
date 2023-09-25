6a0664a2f579438d5a719330f2945b9ed37f510f
wanguiR <wanguikibiru22@gmail.com>
2023-09-25 23:32:54 +0300

    done merge

--- _config.js
+++ _config.js
@@ -2,26 +2,9 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
     production: 'mongodb+srv://rosewangui:6H48jbhlyfZauPlE@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
     development: 'mongodb+srv://rosewangui:6H48jbhlyfZauPlE@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
     test: 'mongodb+srv://rosewangui:6H48jbhlyfZauPlE@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
-=======
-    production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
->>>>>>> test
-=======
-    production: 'mongodb+srv://rosewangui:6H48jbhlyfZauPlE@cluster0.p7y1pkl.mongodb.net/?retryWrites=true&w=majority',
-    //development: 'mongodb+srv://rosewangui:6H48jbhlyfZauPlE@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    //test: 'mongodb+srv://rosewangui:6H48jbhlyfZauPlE@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
->>>>>>> ced080af89051a1212b95ef02d36bf49d15d2e01
-=======
-    production: 'mongodb+srv://adminDB:7npscgwzXlBjsnc@gallery.dc3g6oq.mongodb.net/?retryWrites=true&w=majority',
-    //development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    //test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
->>>>>>> origin/test
+
 }
 module.exports = config;
